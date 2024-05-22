c097c76e76f3cccf00a116cfc53d7c23cc9965d3
ovireakboth <bothzin007@gmail.com>
2024-05-22 15:25:10 +0700

    first commit

---
+++ exercsie_1.js
@@ -0,0 +1,14 @@
+
+// create a program to display shape below
+let n = 5;
+for (let i = n; i >= 1; i--) {
+    let str = "*";
+    console.log(str.repeat(i));
+    
+}
+
+// Merge 2 arrays into one single array
+let arr1 = [0,1,2,3,4];
+let arr2 = [5,6,7,8,9];
+mArr = arr1.concat(arr2);
+console.log(mArr);
